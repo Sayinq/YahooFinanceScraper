1466dd487a93863ad52514a0effcd5a455f0c456
MylesGit <mylesmcghee1999@yahoo.com>
2023-03-21 19:32:47 -0400

    lib not grabbing daily keys to decrypt - abandoned

--- SSGUITest.py
+++ SSGUITest.py
@@ -1,49 +1,37 @@
-import yfinance as yf
-import pandas as pd
-import matplotlib.pyplot as plt
-from pandas_datareader import data
-import ta
-import plotly.graph_objs as go
-import plotly.offline as pyo
-import tkinter as tk
-from tkinter import ttk
-APPLE = yf.Ticker("AAPL")
+#Holding for reconstruction
 
-#Date parameters that we want to see in our plot
-start_date = '2021-02-02'
-end_date = '2022-01-01'
+#Heiken Aishi Candlestick Chart
+HeikenAishi = go.Candlestick(x=stockpanel.index,
+                            open=stockpanel['HA_Open'],
+                            high=stockpanel['HA_High'],
+                            low=stockpanel['HA_Low'],
+                            close=stockpanel['HA_Close'],
+                            name= StockTicker)
+#Candle Stick Plot
+#Candlestick = go.Candlestick(x=stockpanel.index,
+#                    open=stockpanel['Open'],
+#                    high=stockpanel['High'],
+#                    low=stockpanel['Low'],
+#                    close=stockpanel['Close'],
+#                    name= StockTicker)
+#Tracing variables for our Upper Band and Lower Band, in which case we made green for visibility testing
+UpperBoll = go.Scatter(x = stockpanel.index,
+                    y=stockpanel['Upper Band'],
+                    name= 'Upper Band',
+                    line=dict(color="Grey"))
 
-#Finding the history within our date parameters we set.
-stockpanel = APPLE.history(start=start_date, end=end_date)
-
-def open_line_chart():
-    fig, ax = plt.subplots(figsize=(10,5))
-    stockpanel[['Close', 'RSI']].plot(ax=ax)
-    ax.set_title("Apple Inc. Stock Price")
-    ax.set_ylabel("Closing Price")
-    ax.set_xlabel("Date")
-    plt.show()
-
-def open_candlestick_chart():
-    trace = go.Candlestick(x=stockpanel.index,
-                       open=stockpanel['Open'],
-                       high=stockpanel['High'],
-                       low=stockpanel['Low'],
-                       close=stockpanel['Close'])
-    data = [trace]
-    layout = go.Layout(title='Apple Inc. Stock Price (Candlestick)',
-                   xaxis=dict(title='Date'),
-                   yaxis=dict(title='Closing Price'))
-    fig = go.Figure(data=data, layout=layout)
-    pyo.plot(fig, filename='candlestick-chart.html',auto_open=True, show_link=False)
-
-root = tk.Tk()
-root.title("Stock Chart")
-
-line_chart_button = ttk.Button(root, text = "Line Chart", command = open_line_chart)
-line_chart_button.pack()
-
-candlestick_chart_button = ttk.Button(root, text = "Candlestick Chart", command = open_candlestick_chart)
-candlestick_chart_button.pack()
-
-root.mainloop()
+LowerBoll = go.Scatter(x = stockpanel.index,
+                    y=stockpanel['Lower Band'],
+                    name= 'Lower Band',
+                    line=dict(color="Grey"))
+#Tracing variables for MACD
+MACD = go.Scatter(x=stockpanel.index, y=stockpanel['MACD'],
+                name='MACD',
+                line=dict(color='blue'))
+Signal = go.Scatter(x=stockpanel.index, y=stockpanel['Signal'],
+                name='Signal',
+                line=dict(color='yellow'),
+                row=2, col=1)
+                
+#Data that's being pulled, as well as Indicators
+data = [HeikenAishi, UpperBoll, LowerBoll]
--- StockScraping.py
+++ StockScraping.py
@@ -15,20 +15,13 @@ import mplfinance as mpf
 
 #Target stock(s)
 #Expand, post multiple?
-#Implement Nasdaq Data Link API / Nasdaq Last Sale API for 10sec delay for real-time updatable data? 
-#Try/Except loop so it won't explode
 StockTicker = ''
-while StockTicker == '':
-    try:
-        APPLE = yf.Ticker(input("Which stock ticker would you like to analyze?: ")) 
-        StockTicker = APPLE.info['shortName'] #User input test
+stock = yf.Ticker(input("Which stock ticker would you like to analyze?: ")) 
+StockTicker = stock.info['shortName'] #User input test
     #Date parameters that we want to see in our plot
-        start_date = '2021-02-02'
-        end_date = '2022-01-01'
-        stockpanel = APPLE.history(start=start_date, end=end_date) #Displaying within date range
-        Cleaning = stockpanel.dropna(subset=['Open', 'High', 'Low', 'Close'], how= 'all') #Cleaning possible NaN values (ew)
-    except yf.exceptions.YFinanceError:
-        print("Invalid smybol, please try again.")
+start_date = '2021-02-02'
+end_date = '2022-01-01'
+stockpanel = stock.history(start=start_date, end=end_date) #Displaying within date range
 
 #Plotting it (Line Chart)
 #Try to make interactable in some way? 
@@ -43,8 +36,8 @@ while StockTicker == '':
 #Using 'ta' Library so I can grab trend indicators for Bollinger Bands
 stockpanel['MA'] = ta.trend.sma_indicator(stockpanel['Close'])
 stockpanel['STD'] = stockpanel['Close'].rolling(window=10).std()
-stockpanel['Upper Band'] = stockpanel['MA'] + 2*stockpanel['STD']
-stockpanel['Lower Band'] = stockpanel['MA'] - 2*stockpanel['STD']
+stockpanel['Upper Band'] = stockpanel['MA'] + 2 * stockpanel['STD']
+stockpanel['Lower Band'] = stockpanel['MA'] - 2 * stockpanel['STD']
 
 #Using 'ta' library to implement a MACD line (Moving Average Convergence/Divergence Indicator)
 macd_diff = ta.trend.macd_diff(stockpanel['Close'], window_fast=20, window_slow=100)
@@ -110,16 +103,6 @@ fig.add_trace(LowerBoll)
 fig.add_trace(MACD)
 fig.add_trace(Signal)
 fig.update_layout(xaxis_rangeslider_visible=True)
-fig.update_layout(title=f'{StockTicker} Stock Ticker',
-                xaxis_title='Date',
-                yaxis_title='Closing Price',
-                hovermode='x',
-                annotations=[dict(text='Bollinger Bands Analysis',
-                x=0.5, y=1.05,
-                xref='paper',
-                yref='paper',
-                showarrow=False, align='center')]
-)
 with open("Candlestick.html", "w", encoding="utf-8") as f:
     fig.write_html(f, auto_open=True)
 webbrowser.open("Candlestick.html")
